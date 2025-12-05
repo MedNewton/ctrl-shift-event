38f1b26260f7e2664a47e96685ba76a90aa0d89e
MedNewton <med.newton.2013@gmail.com>
2025-12-05 19:40:14 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
-  import { imagetools } from "vite-imagetools";
   import path from 'path';
   import viteImagemin from "vite-plugin-imagemin";
 
   export default defineConfig({
     plugins: [
-      react(), 
-      imagetools(),
+      react(),
       viteImagemin({
         // you can tweak these later; this is a reasonable starting point
         gifsicle: {
