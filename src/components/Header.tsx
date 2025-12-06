bb73ffe3c7b20c9171734ecf6e0b0d669ae3b50e
MedNewton <med.newton.2013@gmail.com>
2025-12-06 05:22:56 +0100

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { Instagram, Linkedin, Menu, X } from "lucide-react";
+import { FaXTwitter, FaInstagram, FaLinkedin, FaTelegram, FaBars, FaX } from "react-icons/fa6";
 import { ImageWithFallback } from "./figma/ImageWithFallback";
 import logoImg from "../assets/images/logo.webp";
 import { motion, AnimatePresence } from "motion/react";
@@ -11,51 +11,22 @@ import { AboutMenu } from "./AboutMenu";
 const SOCIAL_LINKS = [
   {
     name: "X",
-    icon: (
-      <svg
-        role="img"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4 fill-current transition-colors group-hover:text-[#DCB821]"
-      >
-        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-      </svg>
-    ),    
+    icon: <FaXTwitter className="h-5 w-5" />,
     href: "#",
   },
   {
     name: "Instagram",
-    icon: (
-      <Instagram className="h-4 w-4 transition-colors group-hover:text-[#DCB821]" />
-    ),
+    icon: <FaInstagram className="h-5 w-5" />,
     href: "#",
   },
   {
     name: "Linkedin",
-    icon: (
-      <svg
-        role="img"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4 fill-current transition-colors group-hover:text-[#DCB821]"
-      >
-        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452z" />
-      </svg>
-    ),
+    icon: <FaLinkedin className="h-5 w-5" />,
     href: "#",
   },
   {
     name: "Telegram",
-    icon: (
-      <svg
-        role="img"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4 fill-current transition-colors group-hover:text-[#DCB821]"
-      >
-        <path d="M21.684 1.97L2.133 9.756c-1.41.58-1.403 1.39.247 1.907l5.165 1.663 11.793-7.54c.577-.393 1.12-.185.678.213l-9.715 8.92-.368 5.43c.53 0 .765-.247 1.063-.54l2.564-2.53 5.35 4.02c.987.556 1.704.272 1.95-1.13L24.632 3.52c.347-1.67-.626-2.44-1.63-2.44-1.12 0-1.318.89-1.318.89z" />
-      </svg>
-    ),
+    icon: <FaTelegram className="h-5 w-5" />,
     href: "#",
   },
 ];
@@ -131,7 +102,7 @@ export function Header({
           className="md:hidden flex items-center justify-center text-white"
           onClick={() => setIsOpen(true)}
         >
-          <Menu className="h-6 w-6" />
+          <FaBars className="h-6 w-6" />
         </button>
       </div>
 
@@ -157,7 +128,7 @@ export function Header({
                 className="flex items-center justify-center text-white"
                 onClick={() => setIsOpen(false)}
               >
-                <X className="h-6 w-6" />
+                <FaX className="h-6 w-6" />
               </button>
             </div>
 
